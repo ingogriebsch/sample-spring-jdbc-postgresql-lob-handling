7adecfe9d518760f4deb97ae8f496e0484654027
Uwe Schaefer <uwe@codesmell.de>
2019-07-29 21:11:47 +0200

    minor lombok simplification

--- src/main/java/com/github/ingogriebsch/sample/spring/jdbc/postgresql/lob/handling/BlobUtils.java
+++ src/main/java/com/github/ingogriebsch/sample/spring/jdbc/postgresql/lob/handling/BlobUtils.java
@@ -3,33 +3,25 @@ package com.github.ingogriebsch.sample.spring.jdbc.postgresql.lob.handling;
 import static lombok.AccessLevel.PRIVATE;
 
 import java.sql.Blob;
-import java.sql.SQLException;
 
 import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
 
 // FIXME Only necessary if we use a Blob for the content.
 @NoArgsConstructor(access = PRIVATE)
 public final class BlobUtils {
 
+    @SneakyThrows
     public static void free(Blob blob) {
         if (blob == null) {
             return;
         }
-
-        try {
-            blob.free();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        blob.free();
     }
 
     public static void freeQuietly(Blob blob) {
-        if (blob == null) {
-            return;
-        }
-
         try {
-            blob.free();
+            free(blob);
         } catch (Exception e) {
             // nothing to do
         }
